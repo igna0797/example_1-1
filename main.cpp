5640a05567d9bb725662abe98bf063b633d1dd99
igna0797 <igna0797@gmail.com>
2022-09-02 19:20:47 +0000

    Blinqui cuando detecta

--- main.cpp
+++ main.cpp
@@ -3,21 +3,30 @@
 
 int main()
 {
+
     DigitalIn gasDetector(D2);
-
-    DigitalOut alarmLed(LED1);
-
+    
+    DigitalOut alarmLed(LED2);
+    
+    DigitalInOut buzzer(D3);
+    buzzer.mode(OpenDrain);
+    buzzer.input();    
+    
     gasDetector.mode(PullDown);
 
-    alarmLed = OFF;
-
+    alarmLed=ON;
     while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
+            if (gasDetector.read() ==ON) { 
+                buzzer.output();
+                buzzer = LOW;
+                alarmLed = ON;
+                printf("led ON");
+                ThisThread::sleep_for(500ms);
+                alarmLed = OFF;
+                buzzer.input();
+                printf("led OFF");
+                ThisThread::sleep_for(500ms);
+            }
         
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
-        }
     }
 }
